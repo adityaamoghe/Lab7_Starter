5d9415f0131fbbcb6e1e0703257dd411c8b7bed2
Aditya Moghe <adityaamoghe@gmail.com>
2021-11-05 14:17:14 -0700

    Parts 2 and 3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,19 +23,11 @@ const router = new Router(function () {
    * element.classList.remove())
    * 
    * This will only be two single lines
-   * If you did this right, you should see the recipe cards just like last lab
+   * If you did this right, you should see just 1 recipe card rendered to the screen
    */
 
-  let recCard = document.getElementsByClassName('section--recipe-cards')[0];
-  let recCardExp = document.getElementsByClassName('section--recipe-expand');
-  let size = recCardExp.size();
-
-  recCard.classList.add('shown');
-  recCard.classList.remove('hidden');
-
-  for(let iterator = 0; iterator < size; iterator++){
-    recCardExp.classList.remove('shown');
-  }
+   document.querySelector('section.section--recipe-cards').classList.add('shown');
+   document.querySelector('section.section--recipe-expand').classList.remove('shown');
 
 });
 
@@ -103,27 +95,6 @@ async function fetchRecipes() {
  * Generates the <recipe-card> elements from the fetched recipes and
  * appends them to the page
  */
-function createRecipeCards() {
-  // Makes a new recipe card
-  const recipeCard = document.createElement('recipe-card');
-  // Inputs the data for the card. This is just the first recipe in the recipes array,
-  // being used as the key for the recipeData object
-  recipeCard.data = recipeData[recipes[0]];
-
-  // This gets the page name of each of the arrays - which is basically
-  // just the filename minus the .json. Since this is the first element
-  // in our recipes array, the ghostCookies URL, we will receive the .json
-  // for that ghostCookies URL since it's a key in the recipeData object, and
-  // then we'll grab the 'page-name' from it - in this case it will be 'ghostCookies'
-  const page = recipeData[recipes[0]]['page-name'];
-  router.addPage(page, function() {
-    document.querySelector('.section--recipe-cards').classList.remove('shown');
-    document.querySelector('.section--recipe-expand').classList.add('shown');
-    document.querySelector('recipe-expand').data = recipeData[recipes[0]];
-  });
-  bindRecipeCard(recipeCard, page);
-
-  document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
 
   /**
    * TODO - Part 1 - Step 3
@@ -132,7 +103,32 @@ function createRecipeCards() {
    * understand what it is doing. Then, turn this into a for loop to iterate over 
    * all the recipes. (bonus - add the class 'hidden' to every recipe card with 
    * an index greater  than 2 in your for loop to make show more button functional)
+   * After this step you should see multiple cards rendered like the end of the last
+   * lab
    */
+
+function createRecipeCards() {
+  let size = recipes.length;
+  for(let itr = 0; itr < size; itr++){
+    const recipeCard = document.createElement('recipe-card'); 
+    recipeCard.data = recipeData[recipes[0+itr]];
+
+    if(i != 0 && i > 2){
+      recipeCard.className = 'hidden';
+    }
+  
+    const page = recipeData[recipes[0+itr]]['page-name'];
+    router.addPage(page, function() {
+      document.querySelector('.section--recipe-cards').classList.remove('shown');
+      document.querySelector('.section--recipe-expand').classList.add('shown');
+      document.querySelector('recipe-expand').data = recipeData[recipes[0]];
+    });
+    
+    bindRecipeCard(recipeCard, page);
+    document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
+  
+  }
+
 }
 
 /**
