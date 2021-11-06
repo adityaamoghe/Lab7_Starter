967bcd99012d10c3d177717b37127e3bac964a5d
Aditya Moghe <adityaamoghe@gmail.com>
2021-11-05 17:07:03 -0700

    sw.js is not working offline

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
+let urlsToCache = ["/assets/scripts/main.js", "/assets/scripts/Router.js", "/assets/components/RecipeCard.js", "/assets/components/RecipeExpand.js"];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
@@ -9,6 +10,13 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+   event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +29,22 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+   /*var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+   event.waitUntil(
+     caches.keys().then(function(cacheNames) {
+       return Promise.all(
+         cacheNames.map(function(cacheName) {
+           if (cacheAllowlist.indexOf(cacheName) === -1) {
+             return caches.delete(cacheName);
+           }
+         })
+       );
+     })
+   );*/
+   event.waitUntil(clients.claim());
+
+
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +53,14 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
